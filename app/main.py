7c8b534e136009dfc88a389dc0e635f5337ee9c6
Pawan Upadhyay <pawanupadhyay3002@gmail.com>
2025-08-19 23:25:48 +0530

    add pwd command in main.py

--- app/main.py
+++ app/main.py
@@ -4,40 +4,38 @@ import os
 
 def find_executable(command):
     # Get the PATH environment variable and split it into a list of directories.
-    PATH = os.environ['PATH']
-    PATH_DIRS = PATH.split(":")
+    path_env = os.environ['PATH']
+    path_dirs = path_env.split(":")
     
         # Iterate through each directory in the PATH.
-    for dir in PATH_DIRS:
+    for dir in path_dirs:
         # Skip directories that don't exist.
-        if not os.path.exists(dir):
+        if not os.path.isdir(dir):
             continue
 
         # Construct the full path to the potential command.
-        command_path = os.path.join(dir, command)
+        executable_path = os.path.join(dir, command)
 
-        # Check if the command exists and is executable.
-        if os.path.exists(command_path):
-            if os.access(command_path, os.X_OK):
-                return command_path
+        if os.path.isfile(executable_path) and os.access(executable_path, os.X_OK):
+            return executable_path
     return None
 
 # type command
 def type_command(command):
-    command_path = find_executable(command)
     # Check if the command is a built-in shell command first.
     if command in commands:
         print(f"{command} is a shell builtin")
         return
 
-    elif command_path:
-        print(f"{command} is {command_path}")
-
+    executable_path = find_executable(command)
+    if executable_path:
+        print(f"{command} is {executable_path}")
     # If the command is not found in the PATH, print a "not found" message.
     else: print(f"{command}: not found")
 
-def helper():
-    pass
+
+
+
 
 # dictionary of commands
 commands = {
@@ -47,27 +45,44 @@ commands = {
     'echo' : lambda *args: print(" ".join(args)),
     # lambda command: print(f"{command} is a shell builtin") if command in commands else print(f"{command}: not found")
     'type' : type_command,
+    'pwd': lambda *args: print(os.getcwd())
 }
-
     
 
 def main():
     while True:        
         # standard output in terminal
         sys.stdout.write("$$ ")
+        sys.stdout.flush()
         
         # command from user with arguments
-        command_with_arguments = input().split()
+        try:
+            command_with_arguments = input().split()
+        except EOFError:
+            print()
+            break
         
         # split command to a command and an argument
         command = command_with_arguments[0]
         argument = command_with_arguments[1:]
 
         # command not found
-        if command not in commands:
-            print(f"{command}: command not found")
-            continue
-        commands[command](*argument)
+        if command in commands:
+            commands[command](*argument)
+        else:
+            executable_path = find_executable(command)
+            
+            if executable_path:
+                # child process, near exact copy of our shell
+                pid = os.fork()
+                if pid == 0:
+                # child process
+                    os.execv(executable_path, [command] + argument)
+                else:
+                    # parent process
+                    os.waitpid(pid, 0)
+            else:
+                print(f"{command}: command not found")
     
 if __name__ == "__main__":
     main()
