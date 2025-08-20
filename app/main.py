5e2d77109890d0ebdfe8190ccdafe1b5e52b6296
Pawan Upadhyay <pawanupadhyay3002@gmail.com>
2025-08-21 00:34:50 +0530

    Refactored command handling logic into a separate function handle_input()

--- app/main.py
+++ app/main.py
@@ -1,6 +1,35 @@
 import sys
 import os
 
+def handle_input():
+        # command from user with arguments
+        try:
+            command_with_arguments = input().split()
+        except EOFError:
+            return False
+        
+        # split command to a command and an argument
+        command = command_with_arguments[0]
+        argument = command_with_arguments[1:]
+
+        # command not found
+        if command in commands:
+            commands[command](*argument)
+        else:
+            executable_path = find_executable(command)
+            
+            if executable_path:
+                # child process, near exact copy of our shell
+                pid = os.fork()
+                if pid == 0:
+                # child process
+                    os.execv(executable_path, [command] + argument)
+                else:
+                    # parent process
+                    os.waitpid(pid, 0)
+            else:
+                print(f"{command}: command not found")
+        return True
 
 def find_executable(command):
     # Get the PATH environment variable and split it into a list of directories.
@@ -64,34 +93,9 @@ def main():
         sys.stdout.write("$$ ")
         sys.stdout.flush()
         
-        # command from user with arguments
-        try:
-            command_with_arguments = input().split()
-        except EOFError:
-            print()
-            break
         
-        # split command to a command and an argument
-        command = command_with_arguments[0]
-        argument = command_with_arguments[1:]
-
-        # command not found
-        if command in commands:
-            commands[command](*argument)
-        else:
-            executable_path = find_executable(command)
-            
-            if executable_path:
-                # child process, near exact copy of our shell
-                pid = os.fork()
-                if pid == 0:
-                # child process
-                    os.execv(executable_path, [command] + argument)
-                else:
-                    # parent process
-                    os.waitpid(pid, 0)
-            else:
-                print(f"{command}: command not found")
+        if not handle_input():
+            break
     
 if __name__ == "__main__":
     main()
