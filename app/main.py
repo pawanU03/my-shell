fe518a1c7324434bda6ceddc146bf23fa146455f
Pawan Upadhyay <pawanupadhyay3002@gmail.com>
2025-08-26 23:28:31 +0530

    Added exit command with argument checking and modified existing exit command

--- app/main.py
+++ app/main.py
@@ -77,14 +77,31 @@ def change_directory(*args):
             target_path = args[0]
         
         if os.path.isdir(target_path):
-            return os.chdir(target_path)
+            os.chdir(target_path)
         else:
             print(f"cd: {target_path}: No such file or directory")
+            
+
+# exit the shell with argument checking
+def exit_command(*args):
+    if len(args) > 1:
+        print("exit: too many arguments")
+        return
+    
+    if not args:
+        os._exit(0)
+    
+    try:
+        exit_status = int(args[0])
+        os._exit(exit_status)
+    except ValueError:
+        print(f"exit: {args[0]}: numeric argument required")
+        return
         
 # dictionary of commands
 commands = {
     # exit the shell
-    'exit' : lambda exit_status=0, *_: print(os._exit(exit_status)),
+    'exit' : exit_command,
     # *args: allow to take variable number of arguments
     'echo' : lambda *args: print(" ".join(args)),
     'type' : type_command,
@@ -98,7 +115,6 @@ def main():
         # standard output in terminal
         sys.stdout.write("$$ ")
         sys.stdout.flush()
-        
         
         if not handle_input():
             break
